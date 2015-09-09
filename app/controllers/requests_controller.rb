1c7b3c34c6833ae1c06b0166f3f715f3f55df699
teelu <terrence.lun@gmail.com>
2015-09-09 12:08:24 +0800

    feat(request) - index pick ups with view

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,13 +6,12 @@ class RequestsController < ApplicationController
       user = User.find(user_id)
       request_params = request_create_params
       location = request_params[:source_location].split(',')
-      new_location = Location.new({coordinates:[ location[0], location[1]]})
+      new_location = Location.new({coordinates:[ location[0], location[1]], address: request_params[:address], category: "Random"})
       if new_location.save
         # save successfuly, create the request
-        request_params[:source_location] = new_location
-        #find user
-
-          request = user.requests.create(request_params)
+        request_hash = Hash.new
+        request_hash[:source_location] = new_location
+        @request = user.requests.create(request_hash)
       else
         # location not save
         render json: {error: "Location not saved"}, status: :bad_request
@@ -22,9 +21,12 @@ class RequestsController < ApplicationController
     end
   end
 
+  def index_pick_up
+    @requests = Request.where(status: "request_pick_up")
+  end
 
   private
     def request_create_params
-      params.require(:request).permit(:source_location, :user_id)
+      params.require(:request).permit(:source_location, :address)
     end
 end
