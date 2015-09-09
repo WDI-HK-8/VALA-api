e05e38c2451bddc9f097fe21810efbf9fc8c32fa
teelu <terrence.lun@gmail.com>
2015-09-09 11:11:32 +0800

    help(requests) - request create working

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,16 +1,24 @@
 class RequestsController < ApplicationController
   def create
     #create location entered
-    request_params = request_create_params
-    location = request_params[:source_location].split(',')
-    new_location = Location.new({coordinates:[ location[0], location[1]]})
-    if new_location.save
-      # save successfuly, create the request
-      request_params[:source_location] = new_location
-      request = Request.create(request_params)
-    else
-      # location not save
-      render json: {error: "Location not saved"}, status: :bad_request
+    user_id = params[:user_id]
+    begin
+      user = User.find(user_id)
+      request_params = request_create_params
+      location = request_params[:source_location].split(',')
+      new_location = Location.new({coordinates:[ location[0], location[1]]})
+      if new_location.save
+        # save successfuly, create the request
+        request_params[:source_location] = new_location
+        #find user
+
+          request = user.requests.create(request_params)
+      else
+        # location not save
+        render json: {error: "Location not saved"}, status: :bad_request
+      end
+    rescue ActiveRecord::RecordNotFound 
+        render json: {error: "User does not exist"}, status: :bad_request
     end
   end
 
