4a2480bdd500a6e983ed843d797e614959e96b1b
teelu <terrence.lun@gmail.com>
2015-09-13 23:59:31 +0800

    feat(tracking) - routes in progress

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -1,10 +1,14 @@
 class StaticPagesController < ApplicationController
   def user_location
-
+    user_id = params[:user_id]
+    location = location_params
+    PrivatePub.publish_to "/user/location/#{user_id}", :location => location
   end
 
   def valet_location
-    
+    valet_id = params[:valet_id]
+    location = location_params
+    PrivatePub.publish_to "/valet/location/#{valet_id}", :location => location
   end
 
   private
